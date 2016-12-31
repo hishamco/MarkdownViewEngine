acd27ad876b5038fa77b54f92994ab3883cf54a8
hishamco <hishamco_2007@yahoo.com>
2016-12-31 10:17:19 +0300

    Add markdown page

---
+++ src/MarkdownViewEngine/IMarkdownPage.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MarkdownViewEngine
+{
+    public interface IMarkdownPage
+    {
+        ViewContext ViewContext { get; set; }
+
+        IHtmlContent BodyContent { get; set; }
+
+        string Path { get; set; }
+
+        Task ExecuteAsync();
+    }
+}
---
+++ src/MarkdownViewEngine/MarkdownPage.cs
@@ -0,0 +1,28 @@
+﻿using CommonMark;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MarkdownViewEngine
+{
+    public class MarkdownPage : IMarkdownPage
+    {
+        public IHtmlContent BodyContent { get; set; }
+
+        public string Path { get; set; }
+
+        public ViewContext ViewContext { get; set; }
+
+        public async Task ExecuteAsync()
+        {
+            using (var reader = new StreamReader(File.OpenRead(Path)))
+            {
+                var markdown = await reader.ReadToEndAsync();
+                var html = CommonMarkConverter.Convert(markdown);
+
+                BodyContent = new HtmlString(html);
+            }
+        }
+    }
+}
