033fc4b3d61f47c32828e636e1136c8cb6b9002c
hishamco <hishamco_2007@yahoo.com>
2016-12-31 10:17:58 +0300

    Create IMarkdownViewEngine

---
+++ src/MarkdownViewEngine/IMarkdownViewEngine.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace MarkdownViewEngine
+{
+    public interface IMarkdownViewEngine : IViewEngine
+    {
+        string GetAbsolutePath(string executingFilePath, string pagePath);
+    }
+}
