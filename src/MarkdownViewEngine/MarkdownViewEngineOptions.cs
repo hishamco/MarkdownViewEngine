844a44da1eb627d2d315b8669fe85f4b3d7cdb31
hishamco <hishamco_2007@yahoo.com>
2016-12-31 10:24:56 +0300

    Create and setup MarkdownViewEngineOptions

---
+++ src/MarkdownViewEngine/MarkdownViewEngineOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MarkdownViewEngine
+{
+    public class MarkdownViewEngineOptions
+    {
+        public IList<string> ViewLocationFormats { get; } = new List<string>();
+    }
+}
---
+++ src/MarkdownViewEngine/MarkdownViewEngineOptionsSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace MarkdownViewEngine
+{
+    public class MarkdownViewEngineOptionsSetup : ConfigureOptions<MarkdownViewEngineOptions>
+    {
+        public MarkdownViewEngineOptionsSetup()
+            : base(options => ConfigureMarkdown(options))
+        {
+
+        }
+
+        private static void ConfigureMarkdown(MarkdownViewEngineOptions markdownOptions)
+        {
+            markdownOptions.ViewLocationFormats.Add("Views/{1}/{0}" + MarkdownViewEngine.ViewExtension);
+            markdownOptions.ViewLocationFormats.Add("Views/Shared/{0}" + MarkdownViewEngine.ViewExtension);
+        }
+    }
+}
