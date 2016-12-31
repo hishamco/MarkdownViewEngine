146e50379cd61465564af016e23da58950933826
hishamco <hishamco_2007@yahoo.com>
2016-12-31 10:29:42 +0300

    Using MarkdownViewEngine in the sample

---
+++ samples/MarkdownViewEngineSample/Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MarkdownViewEngineSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index() => View();
+    }
+}
