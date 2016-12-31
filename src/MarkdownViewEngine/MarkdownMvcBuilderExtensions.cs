d1b5e2b54a7dbc3f7bf51f835a0e0e516191d9f1
hishamco <hishamco_2007@yahoo.com>
2016-12-31 10:28:04 +0300

    Add MvcBuilder extensions

---
+++ src/MarkdownViewEngine/MarkdownMvcBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿using MarkdownViewEngine;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class MarkdownMvcBuilderExtensions
+    {
+        public static IMvcBuilder AddMarkdownViewEngine(this IMvcBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            builder.Services.AddOptions();
+            builder.Services.AddTransient<IConfigureOptions<MarkdownViewEngineOptions>, MarkdownViewEngineOptionsSetup>();
+            builder.Services.AddTransient<IConfigureOptions<MvcViewOptions>, MarkdownMvcViewOptionsSetup>();
+            builder.Services.AddSingleton<IMarkdownViewEngine, MarkdownViewEngine.MarkdownViewEngine>();
+
+            return builder;
+        }
+
+        public static IMvcBuilder AddMarkdownViewEngine(
+            this IMvcBuilder builder,
+            Action<MarkdownViewEngineOptions> setupAction)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            if (setupAction == null)
+            {
+                throw new ArgumentNullException(nameof(setupAction));
+            }
+
+            builder.Services.AddOptions();
+            builder.Services.AddTransient<IConfigureOptions<MarkdownViewEngineOptions>, MarkdownViewEngineOptionsSetup>();
+            builder.Services.Configure(setupAction);
+            builder.Services.AddTransient<IConfigureOptions<MvcViewOptions>, MarkdownMvcViewOptionsSetup>();
+            builder.Services.AddSingleton<IMarkdownViewEngine, MarkdownViewEngine.MarkdownViewEngine>();
+
+            return builder;
+        }
+    }
+}
---
+++ src/MarkdownViewEngine/MarkdownMvcViewOptionsSetup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace MarkdownViewEngine
+{
+    public class MarkdownMvcViewOptionsSetup : IConfigureOptions<MvcViewOptions>
+    {
+        private readonly IMarkdownViewEngine _markdownViewEngine;
+
+        public MarkdownMvcViewOptionsSetup(IMarkdownViewEngine markdownViewEngine)
+        {
+            if (markdownViewEngine == null)
+            {
+                throw new ArgumentNullException(nameof(markdownViewEngine));
+            }
+
+            _markdownViewEngine = markdownViewEngine;
+        }
+
+        public void Configure(MvcViewOptions options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            options.ViewEngines.Add(_markdownViewEngine);
+        }
+    }
+}
